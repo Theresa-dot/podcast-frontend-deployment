4a267e4aa617038dcf6accd02f916a6eadb096ae
Theresa Jacob <70203086+Theresa-dot@users.noreply.github.com>
2023-11-20 15:21:01 +0530

    Update registrantList.js

--- src/components/registrantList.js
+++ src/components/registrantList.js
@@ -8,7 +8,7 @@ function RegistrantList({ loggedInUser, setNotLoggedIn }) {
 
 
     useEffect(() => {
-        Axios.get("http://localhost:4000/registerRoute")
+        Axios.get("https://podcast-deployment-backend.onrender.com/registerRoute")
             .then((res) => {
                 if (res.status === 200)
                     setArr(res.data)
